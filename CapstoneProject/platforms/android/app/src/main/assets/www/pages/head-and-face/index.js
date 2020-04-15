82e3553cbc32984b5a03d4dc60bdfede368f2f23
PensiveParrot <54560136+pensiveparrot@users.noreply.github.com>
2020-04-15 12:42:53 -0400

    corrections made
    corrections made

--- CapstoneProject/platforms/android/app/src/main/assets/www/pages/head-and-face/index.js
+++ CapstoneProject/platforms/android/app/src/main/assets/www/pages/head-and-face/index.js
@@ -1,105 +1,30 @@
-function pageApprehension() {
-    window.location = "01-apprehension-test.html";
+function pageChvosteksSign() {
+    window.location = "01-chvosteks-sign.html";
 }
 
-function pageRelocation() {
-    window.location = '02-relocation-test.html';
+function pageLoadingTest() {
+    window.location = '02-loading-test.html';
 }
 
-function pageSurprise() {
-    window.location = '03-surprise-test.html';
+function pageBiteTest() {
+    window.location = '03-bite-test.html';
 }
 
-function pageAcroTraction() {
-    window.location = '04-acromioclavicular-traction.html';
+function pagePalpationTest() {
+    window.location = '04-palpation-test.html';
 }
 
-function pageAcroCompression() {
-    window.location = '05-acromioclavicular-compression.html';
+function pageRinneTest() {
+    window.location = '05-rinne-test.html';
 }
 
-function pageOppositeShoulder() {
-    window.location = '06-opposite-shoulder.html';
+function pageWeberTest() {
+    window.location = '06-weber-test.html';
 }
 
-function pagePianoKeySign() {
-    window.location = '07-piano-key-sign.html';
-}
-
-function pagePosteriorApprehension() {
-    window.location = '08-posterior-apprehension.html';
-}
-
-function pageJerkTest() {
-    window.location = '09-jerk-test.html';
-}
-
-function pageSulcusSign() {
-    window.location = '10-sulcus-sign.html';
-}
-
-function pageNeerImpingement() {
-    window.location = '11-neer-impingement.html';
-}
-
-function pageHawkinsKennedy() {
-    window.location = '12-hawkins-kennedy.html';
-}
-
-function pageDropArm() {
-    window.location = '13-drop-arm.html';
-}
-
-function pageEmptyCan() {
-    window.location = '14-empty-can.html';
-}
-
-function pageOpenCan() {
-    window.location = '15-open-can.html';
-}
-
-function pageYergasons() {
-    window.location = '16-yergasons.html';
-}
-
-function pageSpeeds() {
-    window.location = '17-speeds.html';
-}
-
-function pageLudingtons() {
-    window.location = '18-ludingtons.html';
-}
-
-function pageObrien() {
-    window.location = '19-obrien.html';
-}
-
-function pageGrind() {
-    window.location = '20-grind.html';
-}
-
-function pageGerber() {
-    window.location = '21-gerber.html';
-}
-
-document.getElementById("apprehension-test").addEventListener("click", pageApprehension);
-document.getElementById("relocation-test").addEventListener("click", pageRelocation);
-document.getElementById("surprise-test").addEventListener("click", pageSurprise);
-document.getElementById("acromioclavicular-traction").addEventListener("click", pageAcroTraction);
-document.getElementById("acromioclavicular-compression").addEventListener("click", pageAcroCompression);
-document.getElementById("opposite-shoulder").addEventListener("click", pageOppositeShoulder);
-document.getElementById("piano-key-sign").addEventListener("click", pagePianoKeySign);
-document.getElementById("posterior-apprehension").addEventListener("click", pagePosteriorApprehension);
-document.getElementById("jerk-test").addEventListener("click", pageJerkTest);
-document.getElementById("sulcus-sign").addEventListener("click", pageSulcusSign);
-document.getElementById("neer-impingement").addEventListener("click", pageNeerImpingement);
-document.getElementById("hawkins-kennedy").addEventListener("click", pageHawkinsKennedy);
-document.getElementById("drop-arm").addEventListener("click", pageDropArm);
-document.getElementById("empty-can").addEventListener("click", pageEmptyCan);
-document.getElementById("open-can").addEventListener("click", pageOpenCan);
-document.getElementById("yergasons").addEventListener("click", pageYergasons);
-document.getElementById("speeds").addEventListener("click", pageSpeeds);
-document.getElementById("ludingtons").addEventListener("click", pageLudingtons);
-document.getElementById("obrien").addEventListener("click", pageObrien);
-document.getElementById("grind").addEventListener("click", pageGrind);
-document.getElementById("gerber").addEventListener("click", pageGerber);
+document.getElementById("chvosteks-sign").addEventListener("click", pageChvosteksSign);
+document.getElementById("loading-test").addEventListener("click", pageLoadingTest);
+document.getElementById("bite-test").addEventListener("click", pageBiteTest);
+document.getElementById("palpation-test").addEventListener("click", pagePalpationTest);
+document.getElementById("rinne-test").addEventListener("click", pageRinneTest);
+document.getElementById("weber-test").addEventListener("click", pageWeberTest);
