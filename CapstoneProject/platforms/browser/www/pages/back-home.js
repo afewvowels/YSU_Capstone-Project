a8ad623bffc12a2a9aca7362b97c2f381fe7a890
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2020-04-17 11:57:39 -0400

    add back button
    added back button to all relevant pages

---
+++ CapstoneProject/platforms/browser/www/pages/back-home.js
@@ -0,0 +1,5 @@
+function backButtonHome() {
+    window.location = "../../index.html";
+}
+
+document.getElementById("back-button-home").addEventListener("click", backButtonHome);
---
+++ CapstoneProject/platforms/browser/www/pages/back-system.js
@@ -0,0 +1,5 @@
+function backButtonSystem() {
+    window.location = "home.html";
+}
+
+document.getElementById("back-button-system").addEventListener("click", backButtonSystem);
